fa0ed289180c1d40de837368368cdafa2f471abf
Jay Levin <jaylevin96@gmail.com>
2023-04-14 14:04:32 -0500

    associations for spots and reviews

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' })
+      Spot.hasMany(models.Review, { foreignKey: 'spotId' })
     }
   }
   Spot.init({
