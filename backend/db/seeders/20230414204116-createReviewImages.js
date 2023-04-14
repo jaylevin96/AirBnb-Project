c17de87524c0947dbff2d970770743a25bbe34eb
Jay Levin <jaylevin96@gmail.com>
2023-04-14 15:47:02 -0500

    added reviewImage seeds

--- backend/db/seeders/20230414204116-createReviewImages.js
+++ backend/db/seeders/20230414204116-createReviewImages.js
@@ -3,7 +3,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
-options.tableName = 'SpotImages'
+options.tableName = 'ReviewImages'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -18,24 +18,20 @@ module.exports = {
     */
     await queryInterface.bulkInsert(options, [
       {
-        spotId: 1,
-        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-720426520237441315/original/87fcd83e-27a9-4631-89fa-cefea5071c27.jpeg?im_w=1200',
-        preview: true
+        reviewId: 1,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-720426520237441315/original/3995dd85-ea71-4eb7-9380-6a4413d109eb.jpeg?im_w=1440'
       },
       {
-        spotId: 2,
-        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53669775/original/89fe9be9-f035-48f2-9f78-095ad28bbdec.jpeg?im_w=1200',
-        preview: true
+        reviewId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53669775/original/04471436-b37c-4846-84a4-ea83d871c95c.jpeg?im_w=1440'
       },
       {
-        spotId: 3,
-        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/f63462e1-e6f4-4a0a-bf38-87539464e014.jpeg?im_w=1200',
-        preview: true
+        reviewId: 3,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/c4a2ba3f-da40-4b8e-ab45-f08cfa40ea3d.jpeg?im_w=1440'
       },
       {
-        spotId: 4,
-        url: 'https://a0.muscache.com/im/pictures/01f12e45-69c5-4dad-b160-a9770a26a8c8.jpg?im_w=1440',
-        preview: true
+        reviewId: 4,
+        url: 'https://content.r9cdn.net/rimg/himg/64/c2/3a/expediav2-5368781-3321499987-335910.jpg?width=1200&height=630&crop=true'
       }
     ])
   },
@@ -53,6 +49,5 @@ module.exports = {
         [Op.in]: [1, 2, 3, 4]
       }
     })
-
   }
 };
