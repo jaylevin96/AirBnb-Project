2aeaee8a717716c7ef7112dbb0734675156b74a1
Jay Levin <jaylevin96@gmail.com>
2023-04-14 16:11:58 -0500

    created spots router path and udpated index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js')
 const sessionRouter = require('./session.js')
 const usersRouter = require('./users.js')
+const spotsRouter = require('./spots')
 // const { requireAuth } = require('../../utils/auth.js')
 // const { setTokenCookie } = require('../../utils/auth.js')
 // const { User } = require('../../db/models')
@@ -12,6 +13,7 @@ const usersRouter = require('./users.js')
 router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter)
+router.use('/spots', spotsRouter)
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
