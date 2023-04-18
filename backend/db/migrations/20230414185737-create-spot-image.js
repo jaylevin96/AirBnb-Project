2722e85d7f2350b02eac31d54b6ec55991b65dce
Jay Levin <jaylevin96@gmail.com>
2023-04-18 16:10:09 -0500

    fixing cascade again

--- backend/db/migrations/20230414185737-create-spot-image.js
+++ backend/db/migrations/20230414185737-create-spot-image.js
@@ -18,7 +18,7 @@ module.exports = {
         references: {
           model: "Spots"
         },
-        onDelete: 'cascade'
+        onDelete: 'CASCADE'
       },
       url: {
         type: Sequelize.STRING
