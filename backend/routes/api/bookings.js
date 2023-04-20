86fbf219b681e333db4a6cfe8a9cfd6ca74ed4ec
Jay Levin <jaylevin96@gmail.com>
2023-04-20 15:21:05 -0500

    dates

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -22,8 +22,8 @@ router.get('/current', requireAuth, async (req, res) => {
     let images = await SpotImage.findAll({ where: { preview: true }, raw: true })
     for (let booking of Bookings) {
 
-        booking.startDate = booking.startDate.slice(0, 10);
-        booking.endDate = booking.endDate.slice(0, 10);
+        // booking.startDate = booking.startDate.slice(0, 10);
+        // booking.endDate = booking.endDate.slice(0, 10);
         // booking.dataValues.startDate = booking.dataValues.startDate.toISOString().slice(0, 10)
         // booking.dataValues.endDate = booking.dataValues.endDate.toISOString().slice(0, 10)
         // let spot = booking.dataValues.Spot.dataValues;
