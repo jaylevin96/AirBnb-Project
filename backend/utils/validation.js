1ee63c0dfb6c92ecc1c83a2e02ab1b20062262a2
Jay Levin <jaylevin96@gmail.com>
2023-04-20 16:47:15 -0500

    fixing query

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -4,7 +4,9 @@ const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req)
     if (!validationErrors.isEmpty()) {
         const errors = {};
-        validationErrors.array().forEach(error => errors[error.param] = error.msg)
+        validationErrors.array().forEach(error => {
+            errors[error.param] = error.msg
+        })
         const err = Error("Bad request.");
         err.errors = errors;
         err.status = 400;
