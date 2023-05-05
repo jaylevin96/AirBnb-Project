356260a3dd03509767d70e125e377ae98b37ff05
Jay Levin <jaylevin96@gmail.com>
2023-05-05 12:26:17 -0500

    finish auth me

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -22,9 +22,9 @@ router.use('/reviews', reviewsRouter)
 router.use('/bookings', bookingsRouter)
 router.use('/spot-images', spotImagesRouter)
 router.use('/review-images', reviewImagesRouter)
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
 // router.get('/restore-user', (req, res) => {
 //     return res.json(req.user)
 // })
