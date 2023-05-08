0514939f9a9a68ad8d216645513f97a15f08676c
Jay Levin <jaylevin96@gmail.com>
2023-05-08 12:03:46 -0500

    created tiles for all spots

--- frontend/src/components/spots/SpotContainer.js
+++ frontend/src/components/spots/SpotContainer.js
@@ -13,15 +13,18 @@ export default function SpotContainer({ spot }) {
                 history.push(`/spots/${spot.id}`)
             }}>
             <img src={spot.previewImage} />
-            <span>
-                {spot.city},{spot.state}
-            </span>
-            <span>
-                <i className="fa-solid fa-star"></i>
+            <div className="tile-content">
+                <span className="tile-content-info">
+                    {spot.city}, {spot.state}
+                </span>
+                <span className="tile-content-info">
+                    ${spot.price} night
+                </span>
+                <span className="tile-content-review">
+                    <i className="fa-solid fa-star"></i>
 
-                {spot.avgRating}</span>
-            <div>
-                ${spot.price} night
+                    {spot.avgRating}</span>
+
             </div>
 
         </div>
--- frontend/src/components/spots/index.js
+++ frontend/src/components/spots/index.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAllSpots } from "../../store/spots";
 import SpotContainer from './SpotContainer'
+import './spots.css'
 export default function AllSpots() {
     const dispatch = useDispatch();
     const spots = Object.values(useSelector((state) => state.allSpots))
@@ -11,7 +12,7 @@ export default function AllSpots() {
 
 
     return (
-        <>
+        <div id="spots-grid">
             {/* {spots.map((spot) => {
                 return <li key={spot.id}>{spot.name}</li>
             })} */}
@@ -20,7 +21,7 @@ export default function AllSpots() {
             })}
 
 
-        </>
+        </div>
     )
 
 
