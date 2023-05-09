ddbd1a99cc680f419d5572ec33069ca3c2edab5e
Jay Levin <jaylevin96@gmail.com>
2023-05-08 23:11:12 -0500

    css for review modal

--- frontend/src/components/reviews/ReviewModal.js
+++ frontend/src/components/reviews/ReviewModal.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { useModal } from "../../context/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { postReviewsThunk, getReviewsThunk } from "../../store/reviews";
+import "./reviewModal.css"
 export default function ReviewModal() {
     const [review, setReview] = useState('');
     const [stars, setStars] = useState(0)
@@ -10,12 +11,13 @@ export default function ReviewModal() {
     const spot = useSelector((state) => state.spots.singleSpot)
 
     return (
-        <div>
+        <div id="container">
             <h1>How was your stay?</h1>
-            <input type="text" placeholder="Leave your review here..."
+            <textarea placeholder="Leave your review here..."
                 value={review}
-                onChange={(e) => setReview(e.target.value)}></input>
-            <div>
+
+                onChange={(e) => setReview(e.target.value)}></textarea>
+            <div className="stars">
                 <i className={stars >= 1 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
                     onClick={() => {
                         setStars(1)
