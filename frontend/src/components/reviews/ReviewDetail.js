9c8b4898e0ddbd5ad994298f19859899895e1377
Jay Levin <jaylevin96@gmail.com>
2023-05-08 23:32:22 -0500

    css for reviews

--- frontend/src/components/reviews/ReviewDetail.js
+++ frontend/src/components/reviews/ReviewDetail.js
@@ -16,10 +16,24 @@ export default function ReviewDetail({ reviewId, spotId }) {
     }
     const year = createdAt.slice(0, 4)
     const month = createdAt.slice(5, 7)
+    const months = {
+        "01": "January",
+        "02": "February",
+        "03": "March",
+        "04": "April",
+        "05": "May",
+        "06": "June",
+        "07": "July",
+        "08": "August",
+        "09": "September",
+        "10": "October",
+        "11": "November",
+        "12": "December"
+    }
     return (
         <div>
             <h4>{firstName}</h4>
-            <span>{`${year} ${month} `}</span>
+            <span className="review-timestamp">{`${months[month]} ${year}  `}</span>
             <span>{`${stars} stars`}</span>
             <p>{review}</p>
         </div>
