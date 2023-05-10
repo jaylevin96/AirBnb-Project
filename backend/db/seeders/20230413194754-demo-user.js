6275ddf4227884d45764de6aad032aec527b5097
Jay Levin <jaylevin96@gmail.com>
2023-05-10 16:55:06 -0500

    fixed seed data

--- backend/db/seeders/20230413194754-demo-user.js
+++ backend/db/seeders/20230413194754-demo-user.js
@@ -20,11 +20,16 @@ module.exports = {
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: "Steve",
+        lastName: "Doe",
+
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: "John",
+        lastName: "Smith",
         hashedPassword: bcrypt.hashSync('password3')
       }
     ])
