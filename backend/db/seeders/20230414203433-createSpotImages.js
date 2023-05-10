0779613f4f030ae71016d8f9f7541a4841341b8d
Jay Levin <jaylevin96@gmail.com>
2023-05-10 10:20:02 -0500

    more css on details

--- backend/db/seeders/20230414203433-createSpotImages.js
+++ backend/db/seeders/20230414203433-createSpotImages.js
@@ -49,14 +49,75 @@ module.exports = {
       },
       {
         spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/c4a2ba3f-da40-4b8e-ab45-f08cfa40ea3d.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/d84c9ae2-d8bb-43f9-bedb-d599e9122379.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/8e96a97a-a84d-4571-a45b-49044208b8db.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/65457bd8-df05-4136-98ed-5e63888e18af.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 3,
         url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-54078699/original/f63462e1-e6f4-4a0a-bf38-87539464e014.jpeg?im_w=1200',
         preview: true
       },
       {
         spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/c29b646e-ddec-4b8e-9034-9786f733dd4c.jpg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/9f927ae2-0c52-4321-bb3d-223252135b72.jpg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/cfb5d1cb-cb3a-4b9a-b40f-bf9538181c47.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/ee38c40a-4c80-4116-a919-174b31809883.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 4,
         url: 'https://a0.muscache.com/im/pictures/01f12e45-69c5-4dad-b160-a9770a26a8c8.jpg?im_w=1440',
         preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53669775/original/4c42970d-97b7-4c06-a8d6-01cdd585815e.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53669775/original/5efcc0a5-0c03-4c82-b515-f57b39a66672.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53669775/original/04471436-b37c-4846-84a4-ea83d871c95c.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53669775/original/2e633eba-b1ed-4242-b14c-6062c48957f3.jpeg?im_w=720',
+        preview: false
       }
+
     ])
   },
 
@@ -70,7 +131,7 @@ module.exports = {
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete(options, {
       id: {
-        [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8]
+        [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
       }
     })
 
