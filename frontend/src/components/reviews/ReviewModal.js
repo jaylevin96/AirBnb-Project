84be3245b4cca88a9dee1a41a390c6c172c30ab7
Jay Levin <jaylevin96@gmail.com>
2023-05-11 12:15:54 -0500

    added star hover functionality

--- frontend/src/components/reviews/ReviewModal.js
+++ frontend/src/components/reviews/ReviewModal.js
@@ -7,12 +7,14 @@ import "./reviewModal.css"
 export default function ReviewModal() {
     const [review, setReview] = useState('');
     const [stars, setStars] = useState(0)
-
+    const [activeStars, setActiveStars] = useState(0)
     const [errors, setErrors] = useState({})
     const dispatch = useDispatch();
     const { closeModal } = useModal();
     const spot = useSelector((state) => state.spots.singleSpot)
     const disabled = stars < 1 || review.length < 10
+
+
     return (
         <div id="container">
             <h1>How was your stay?</h1>
@@ -21,35 +23,47 @@ export default function ReviewModal() {
                 value={review}
 
                 onChange={(e) => setReview(e.target.value)}></textarea>
-            <div className="stars">
-                <i className={stars >= 1 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
-                    style={stars >= 1 ? { color: "#ff5a5f" } : {}}
+            <div className="stars"
+
+                onMouseLeave={() => setActiveStars(stars)}>
+                <i className={activeStars >= 1 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
+                    style={activeStars >= 1 ? { color: "#ff5a5f" } : {}}
                     onClick={() => {
                         setStars(1)
-                    }}></i>
-                <i className={stars >= 2 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
+                        setActiveStars(1)
+                    }}
+                    onMouseEnter={() => !stars ? setActiveStars(1) : ''}></i>
+                <i className={activeStars >= 2 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
 
-                    style={stars >= 2 ? { color: "#ff5a5f" } : {}}
+                    style={activeStars >= 2 ? { color: "#ff5a5f" } : {}}
                     onClick={() => {
                         setStars(2)
-                    }}></i>
-                <i className={stars >= 3 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
-                    style={stars >= 3 ? { color: "#ff5a5f" } : {}}
+                        setActiveStars(2)
+                    }}
+                    onMouseEnter={() => !stars ? setActiveStars(2) : ''}></i>
+                <i className={activeStars >= 3 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
+                    style={activeStars >= 3 ? { color: "#ff5a5f" } : {}}
                     onClick={() => {
                         setStars(3)
+                        setActiveStars(3)
                     }}
+                    onMouseEnter={() => !stars ? setActiveStars(3) : ''}
                 ></i>
-                <i className={stars >= 4 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
-                    style={stars >= 4 ? { color: "#ff5a5f" } : {}}
+                <i className={activeStars >= 4 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
+                    style={activeStars >= 4 ? { color: "#ff5a5f" } : {}}
                     onClick={() => {
                         setStars(4)
+                        setActiveStars(4)
                     }}
+                    onMouseEnter={() => !stars ? setActiveStars(4) : ''}
                 ></i>
-                <i className={stars >= 5 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
-                    style={stars >= 5 ? { color: "#ff5a5f" } : {}}
+                <i className={activeStars >= 5 ? "fa-solid fa-star fa-lg" : "fa-regular fa-star fa-lg"}
+                    style={activeStars >= 5 ? { color: "#ff5a5f" } : {}}
                     onClick={() => {
                         setStars(5)
+                        setActiveStars(5)
                     }}
+                    onMouseEnter={() => !stars ? setActiveStars(5) : ''}
                 ></i>
                 <span> Stars</span>
 
